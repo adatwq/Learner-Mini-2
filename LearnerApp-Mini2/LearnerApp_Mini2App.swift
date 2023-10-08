77c755007fb6e813612c993410e64305ea8236d5
reem-Q <reem.quhal@gmail.com>
2023-10-08 11:37:14 +0300

    Initial Commit

---
+++ LearnerApp-Mini2/LearnerApp_Mini2App.swift
@@ -0,0 +1,17 @@
+//
+//  LearnerApp_Mini2App.swift
+//  LearnerApp-Mini2
+//
+//  Created by Reem Quhal on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearnerApp_Mini2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
